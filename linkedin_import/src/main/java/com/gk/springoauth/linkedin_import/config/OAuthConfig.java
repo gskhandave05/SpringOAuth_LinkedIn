6cd5c33d5b6da67752c64e3a850793dcf5b0336a
Gaurav Somnath Khandave <gskhandve@gmail.com>
2017-08-17 18:18:20 -0500

    LinkedIn profile import using OAuth2 and Springboot

--- linkedin_import/src/main/java/com/gk/springoauth/linkedin_import/config/OAuthConfig.java
+++ linkedin_import/src/main/java/com/gk/springoauth/linkedin_import/config/OAuthConfig.java
@@ -3,15 +3,18 @@
  */
 package com.gk.springoauth.linkedin_import.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,10 +22,11 @@ import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.filter.CompositeFilter;
 
 /**
  * @author gauravkhandave
@@ -30,49 +34,57 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
  */
 @Configuration
 @EnableOAuth2Client
+@ComponentScan
 public class OAuthConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	  OAuth2ClientContext oAuth2ClientContext;
-	
+	OAuth2ClientContext oAuth2ClientContext;
+
 	@Override
-	protected void configure(HttpSecurity http)
-		      throws Exception {
-		    http.antMatcher("/**")
-		        .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
-		        .authorizeRequests()
-		        .antMatchers("/", "/login**", "/webjars/**")
-		        .permitAll()
-		        .anyRequest()
-		        .authenticated();
-		  }
-	
-	private Filter ssoFilter(){
-		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter("/login/linkedin");
-		OAuth2RestTemplate template = new OAuth2RestTemplate(linkedin(), oAuth2ClientContext);
-	    filter.setRestTemplate(template);
-	    filter.setTokenServices(new UserInfoTokenServices(linkedinResource().getUserInfoUri(), linkedin().getClientId()));
-
-	    return filter;
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class).authorizeRequests()
+				.antMatchers("/", "/connect**", "/webjars/**").permitAll().anyRequest().authenticated().and().logout()
+				.logoutSuccessUrl("/").permitAll().and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		;
 	}
-	
-	//@Bean
-	  @ConfigurationProperties("linkedin.client")
-	  OAuth2ProtectedResourceDetails linkedin() {
-	    return new AuthorizationCodeResourceDetails();
-	  }
 
-	  //@Bean
-	  @ConfigurationProperties("linkedin.resource")
-	  ResourceServerProperties linkedinResource() {
-	    return new ResourceServerProperties();
-	  }
-	  
-	  @Bean
-	  public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-	    FilterRegistrationBean bean = new FilterRegistrationBean();
-	    bean.setFilter(filter);
-	    bean.setOrder(-100);
-	    return bean;
-	  }
+	private Filter ssoFilter() {
+		CompositeFilter filter = new CompositeFilter();
+		List<Filter> filters = new ArrayList<>();
+
+		OAuth2ClientAuthenticationProcessingFilter linkedInFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/connect/linkedIn");
+		OAuth2RestTemplate linkedInTemplate = new OAuth2RestTemplate(linkedIn(), oAuth2ClientContext);
+		linkedInFilter.setRestTemplate(linkedInTemplate);
+		UserInfoTokenServices tokenServices = new UserInfoTokenServices(linkedInResource().getUserInfoUri(), linkedIn().getClientId());
+		tokenServices.setRestTemplate(linkedInTemplate);
+		linkedInFilter.setTokenServices(tokenServices);
+
+		filters.add(linkedInFilter);
+
+		filter.setFilters(filters);
+
+		return filter;
+	}
+
+	@Bean
+	@ConfigurationProperties("linkedin.client")
+	public AuthorizationCodeResourceDetails linkedIn() {
+		return new AuthorizationCodeResourceDetails();
+	}
+
+	@Bean
+	@ConfigurationProperties("linkedin.resource")
+	public ResourceServerProperties linkedInResource() {
+		return new ResourceServerProperties();
+	}
+
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean bean = new FilterRegistrationBean();
+		bean.setFilter(filter);
+		bean.setOrder(-100);
+		return bean;
+	}
 }
--- linkedin_import/src/main/java/com/gk/springoauth/linkedin_import/controller/LinkedInProfileImportController.java
+++ linkedin_import/src/main/java/com/gk/springoauth/linkedin_import/controller/LinkedInProfileImportController.java
@@ -3,12 +3,9 @@
  */
 package com.gk.springoauth.linkedin_import.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.linkedin.api.LinkedIn;
-import org.springframework.social.linkedin.api.LinkedInProfile;
-import org.springframework.ui.Model;
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,26 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping(value="/linkedin")
 public class LinkedInProfileImportController {
 	
-	@Autowired
-	private LinkedIn linkedIn;
-	
-	//private UserProfile userProfile;
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String importProfile(Model model){
-		try {
-			if (!linkedIn.isAuthorized()) {
-			return "redirect:/importer/linkedin";
-
-			}
-			}catch (NullPointerException e){
-			return "redirect:/importer/linkedin";
-			}
-		
-		LinkedInProfile profile = linkedIn.profileOperations().getUserProfile();
-		return profile.getFirstName() +"\t"+profile.getLastName()+"\n"+profile.getSummary();
-	}
+	@RequestMapping("/user")
+	  public Principal lookupUser(Principal user) {
+	    return user;
+	  }
 }
